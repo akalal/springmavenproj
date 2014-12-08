d05de1dbfb7618edb5cb282be94c60ac3464e53c
akalal <bms.asha@gmail.com>
2014-12-08 01:08:29 +0000

    first commit

---
+++ test/com/AppTest.java
@@ -0,0 +1,15 @@
+package test.com;
+
+import org.junit.Assert;
+import org.junit.Test;
+import src.com.App; 
+public class AppTest {
+ 
+	@Test
+	public void testLengthOfTheUniqueKey() {
+ 
+		App obj = new App();
+		Assert.assertEquals(36, obj.generateUniqueKey().length());
+ 
+	}
+}
